e823362611e09238640e5156a153f34f4e266ea0
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-16 12:54:32 -0500

    Incorpreate logo toggling into toggleDarkMode function

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,27 @@
 import './css/base.scss';
 
-import './images/arrow.png';
-import './images/coloredCompass.png';
 import './images/lightDarkMode.png';
+import './images/redBlueCompass.png';
+import './images/invertedCompass.png';
+import './images/arrow.png';
 
 const darkLightMode = document.querySelector('.dark-light-mode-js');
 
 darkLightMode.addEventListener('click', toggleDarkMode);
 
+
 function toggleDarkMode() {
   document.querySelector('body').classList.toggle('dark-mode-on');
   darkLightMode.classList.toggle('dark-mode-on');
-  // document.querySelector().toggle('dark-mode-on');
+  toggleLogoDarkMode();
+}
+
+function toggleLogoDarkMode() {
+  const logo = document.querySelector('.logo-js')
+  logo.classList.toggle('dark-mode-on');
+  if (logo.classList.contains('dark-mode-on')) {
+    logo.src = './images/invertedCompass.png';
+  } else {
+    logo.src = './images/redBlueCompass.png';
+  }
 }
