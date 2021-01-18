c6ed53b6a02bd6f56223f545860b8f0b4a48ec40
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-18 11:18:55 -0500

    Create function to manage displays

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,4 +1,21 @@
 const domUpdates = {
+  greetTraveler(traveler) {
+    document.querySelector('.user-js').innerText = traveler.name.split(' ')[0];
+  },
+
+  updateTravelerStats(traveler, tripsData, destinationsData) {
+    document.querySelector('.total-places-js').innerText = traveler.filterMyTrips(tripsData).length;
+    document.querySelector('.total-price-js').innerText = Math.round(traveler.findTotalExpense(tripsData, destinationsData));
+  },
+
+  manageClassList(action, className, element) {
+    if (action === 'add' && element) {
+      element.classList.add(className);
+    } else if (action === 'remove' && element) {
+      element.classList.remove(className);
+    }
+  },
+
   toggleDarkModeUpdates() {
     document.querySelector('body').classList.toggle('dark-mode-on');
     this.toggleLogoDarkMode();
@@ -12,15 +29,6 @@ const domUpdates = {
     } else {
       logo.src = './images/redBlueCompass.png';
     }
-  },
-
-  greetTraveler(traveler) {
-    document.querySelector('.user-js').innerText = traveler.name.split(' ')[0];
-  },
-
-  updateTravelerStats(traveler, tripsData, destinationsData) {
-    document.querySelector('.total-places-js').innerText = traveler.filterMyTrips(tripsData).length;
-    document.querySelector('.total-price-js').innerText = Math.round(traveler.findTotalExpense(tripsData, destinationsData));
   }
 }
 
