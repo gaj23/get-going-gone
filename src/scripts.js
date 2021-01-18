b822b7aa57920b3aa951ee10959ae863def52851
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-18 11:25:57 -0500

    Apply toggling to each go to button

--- src/scripts.js
+++ src/scripts.js
@@ -14,16 +14,24 @@ import domUpdates from './domUpdates';
 
 // ** Button Query Selectors **
 const darkLightModeButton = document.querySelector('.dark-light-mode-js');
+const homeButton = document.querySelector('.go-home-js');
 const currentTripsButton = document.querySelector('.go-to-current-js');
 const plannedTripsButton = document.querySelector('.go-to-planned-js');
 const pastTripsButton = document.querySelector('.go-to-past-js')
-const homeButton = document.querySelector('.go-home-js')
-const nextDestinationButton = document.querySelector('.next-destination-js');
+const nextDestinationButton = document.querySelector('.make-booking-js');
 
 // ** Display Query Selectors **
+const homeDisplay = document.querySelector('.dashboard-js');
+const currentDisplay = document.querySelector('.detailed-current-trips-js');
+const plannedDisplay = document.querySelector('.detailed-planned-trips-js');
+const pastDisplay = document.querySelector('.detailed-past-trips-js');
 
-
+// ** Event Listeners **
 darkLightModeButton.addEventListener('click', toggleDarkMode);
+homeButton.addEventListener('click', showHome);
+currentTripsButton.addEventListener('click', showCurrentDetails);
+plannedTripsButton.addEventListener('click', showPlannedDetails);
+pastTripsButton.addEventListener('click', showPastDetails);
 
 let traveler;
 let travelers = [];
@@ -35,13 +43,38 @@ window.onload = Promise.all([apiCalls.getTravelersData(), apiCalls.getTripsData(
     data[0].travelers.forEach(traveler => travelers.push(traveler))
     data[1].trips.forEach(trip => trips.push(trip))
     data[2].destinations.forEach(destination => destinations.push(destination))
-    instantiation();
+    getData();
   })
 
-function instantiation() {
+function getData() {
   traveler = new Traveler(travelers[7]);
   domUpdates.greetTraveler(traveler);
   domUpdates.updateTravelerStats(traveler, trips, destinations);
+}
+
+function showHome() {
+  domUpdates.manageClassList('remove', 'hidden', homeDisplay);
+  const detailedDisplays = [currentDisplay, plannedDisplay, pastDisplay];
+  detailedDisplays.forEach(display => {
+    if (!display.classList.contains('hidden')) {
+      domUpdates.manageClassList('add', 'hidden', display);
+    };
+  })
+}
+
+function showCurrentDetails() {
+  domUpdates.manageClassList('add', 'hidden', homeDisplay);
+  domUpdates.manageClassList('remove', 'hidden', currentDisplay);
+}
+
+function showPlannedDetails() {
+  domUpdates.manageClassList('add', 'hidden', homeDisplay);
+  domUpdates.manageClassList('remove', 'hidden', plannedDisplay);
+}
+
+function showPastDetails() {
+  domUpdates.manageClassList('add', 'hidden', homeDisplay);
+  domUpdates.manageClassList('remove', 'hidden', pastDisplay);
 }
 
 function toggleDarkMode() {
