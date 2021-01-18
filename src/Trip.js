d353c469588d1f64fb5f28fb1e59ec5475f2e607
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-18 08:35:52 -0500

    Pass test for Trip constructor

--- src/Trip.js
+++ src/Trip.js
@@ -1,5 +1,14 @@
 class Trip {
-
+  constructor(tripObj) {
+    this.id = tripObj.id;
+    this.userID = tripObj.userID;
+    this.destinationID = tripObj.destinationID;
+    this.travelers = tripObj.travelers;
+    this.date = tripObj.date;
+    this.duration = tripObj.duration;
+    this.status = tripObj.status;
+    this.suggestedActivities = tripObj.suggestedActivities;
+  }
 }
 
 export default Trip;
--- test/trip-test.js
+++ test/trip-test.js
@@ -2,4 +2,28 @@ import chai from 'chai';
 const expect = chai.expect;
 
 import Trip from '../src/Trip'
-import trips from './test-data/trips'
+import tripsData from './test-data/trips'
+
+describe.only('Trip', () => {
+  let trip, trip2;
+
+  beforeEach(() => {
+    trip = new Trip(tripsData[0])
+    trip2 = new Trip(tripsData[1])
+  })
+
+  it('Should contain a trip\'s information', () => {
+    expect(trip.id).to.equal(1);
+    expect(trip.userID).to.equal(44);
+    expect(trip.destinationID).to.equal(49);
+    expect(trip.travelers).to.equal(1);
+    expect(trip.date).to.equal('2019/09/16');
+    expect(trip.duration).to.equal(8);
+    expect(trip.status).to.equal('approved');
+    expect(trip.suggestedActivities).to.be.an('array');
+  })
+
+  it('Should calcuate the estimate of a trip\'s price', () => {
+    expect(trip.estimateCost(destinationsData)).to.equal()
+  })
+})
