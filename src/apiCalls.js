9f49b8bbc6db88730debc2e3ecfd3e24f753c09d
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-20 11:10:37 -0500

    Change alerts to console logs

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,7 @@
 const getData = (url) => {
   return fetch(url)
     .then(response => response.json())
-    .catch(err => alert('Apologizes, we are having trouble loading the data'))
+    .catch(err => console.log('Apologizes, we are having trouble loading the data'))
 }
 
 const apiCalls = {
@@ -29,7 +29,7 @@ const apiCalls = {
       .then((data) => {
         return data
       })
-      .catch(err => `Apologizes, we're getting an error: ${err}`)
+      .catch(err => console.log(`Apologizes, we're getting an error: ${err}`))
   }
 }
 
